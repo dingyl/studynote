7f0a32feec0c7469f99c782aadce80a8d9fcd14d
dongTon <1907928206@qq.com>
2017-10-13 19:32:35 +0800

    修改curl,添加comet

--- curl/demo.php
+++ curl/demo.php
@@ -1,27 +1,46 @@
 <?php
+//打印数组函数
 function p($arr){
     echo "<pre>";
     print_r($arr);
     echo "</pre>";
 }
 
-//curl请求接口
-$ch = curl_init();
-$url = "http://www.zixue.it/";
-//设置URL和相应的选项
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+//curl获取网页内容
+function curl_content($url){
+    //curl请求接口
+    $ch = curl_init();
+    //设置URL和相应的选项
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    //设置返回内容，或者直接将内容给浏览器
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //抓取URL并返回抓取内容
+    $temp = curl_exec($ch);
+    // 关闭cURL资源，并且释放系统资源
+    curl_close($ch);
+    return $temp;
+}
 
-//设置返回内容，或者直接将内容给浏览器
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//curl获取所有链接
+function curl_href($url){
+    preg_match_all("/<a href=\"([^\'\"]*)\" [^>]*>(.*?)<\/a>/",curl_content($url),$matchs);
+    $arr = [];
+    foreach($matchs[1] as $k=>$href){
+        //去除掉空连接
+        if(substr($href,0,12)!="javascript:;"){
+            //补全链接地址
+            if(substr($href,0,4)!="http"){
+                $href=$url.$href;
+            }
+            array_push($arr,$href);
+        }
+    }
+    return $arr;
+}
 
-//抓取URL并返回抓取内容
-$content = curl_exec($ch);
 $matchs = [];
-//获取所有的a链接
-//preg_match_all("/(<a[^>]*>)(.*?)(<\/a>)/",$content,$matchs);
-preg_match_all("/<a[^>]*>.*?<\/a>/",$content,$matchs);
-p($matchs);
+$url = "http://www.zixue.it/";
+p(curl_href($url));
 
-// 关闭cURL资源，并且释放系统资源
-curl_close($ch);
+
