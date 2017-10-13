a42f4830c85b0f7fe314c153c22e98149fb8239a
dongTon <1907928206@qq.com>
2017-10-13 15:17:59 +0800

    curl

---
+++ curl/demo.php
@@ -0,0 +1,31 @@
+<?php
+
+function p($arr){
+    echo "<pre>";
+    print_r($arr);
+    echo "</pre>";
+}
+
+//curl请求接口
+$ch = curl_init();
+$url = "http://www.zixue.it/";
+//设置URL和相应的选项
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+//设置返回内容，或者直接将内容给浏览器
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+//抓取URL并返回抓取内容
+$content = curl_exec($ch);
+$matchs = [];
+//获取所有的a链接
+//preg_match_all("/(<a[^>]*>)(.*?)(<\/a>)/",$content,$matchs);
+preg_match_all("/<a[^>]*>.*?<\/a>/",$content,$matchs);
+
+//echo $content;
+
+p($matchs);
+
+// 关闭cURL资源，并且释放系统资源
+curl_close($ch);
