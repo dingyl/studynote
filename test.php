82375c78f87a66a581fb8cf3a06d9a859db9f022
apple <apple@appledeiMac.local>
2017-12-01 21:22:49 +0800

    update elastic

--- test.php
+++ test.php
@@ -1,18 +1,8 @@
 <?php
-print_r($_FILES);
-?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <title>Document</title>
-</head>
-<body>
-<form action="" method="post" enctype="multipart/form-data">
-    <input type="file" name="file">
-    <input type="submit" value="提交">
-</form>
-</body>
-</html>
+$temp = ['match'=>['text'=>'ding']];
+$json = '{"match":{"text":"ding"},"match":{"text":"hello"}}';
+echo json_encode($temp);
+print_r(json_decode($json,true));
+
+var_export($temp);
