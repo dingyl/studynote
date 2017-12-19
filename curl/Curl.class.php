fe27807907b66717e67e59679a71c1692d3c0944
apple <apple@appledeiMac.local>
2017-12-19 17:23:09 +0800

    curl文件更新

--- curl/Curl.class.php
+++ curl/Curl.class.php
@@ -1,9 +1,12 @@
 <?php
-class Curl{
+
+class Curl
+{
     protected $url;
     protected $curl;
 
-    public function __construct($url=''){
+    public function __construct($url = '')
+    {
         $this->curl = curl_init();
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
         $this->setUrl($url);
@@ -14,9 +17,10 @@ class Curl{
      * @param $url
      * @return $this
      */
-    public function setUrl($url){
+    public function setUrl($url)
+    {
         $this->url = $url;
-        curl_setopt($this->curl,CURLOPT_URL, $this->url);
+        curl_setopt($this->curl, CURLOPT_URL, $this->url);
         return $this;
     }
 
@@ -25,7 +29,8 @@ class Curl{
      * @param $header
      * @return $this
      */
-    public function setHeader($header){
+    public function setHeader($header)
+    {
         curl_setopt($this->curl, CURLOPT_HEADER, $header);
         return $this;
     }
@@ -35,16 +40,18 @@ class Curl{
      * @param $agent
      * @return $this
      */
-    public function setUserAgent($agent){
-        curl_setopt($this->curl,CURLOPT_USERAGENT,$agent);
+    public function setUserAgent($agent)
+    {
+        curl_setopt($this->curl, CURLOPT_USERAGENT, $agent);
         return $this;
     }
 
     /**
      * 设置请求来源地址
      */
-    public function setRefererUrl($url){
-        curl_setopt($this->curl,CURLOPT_REFERER,$url);
+    public function setRefererUrl($url)
+    {
+        curl_setopt($this->curl, CURLOPT_REFERER, $url);
         return $this;
     }
 
@@ -53,13 +60,14 @@ class Curl{
      * @param $cookie
      * @return $this
      */
-    public function setCookie($cookies){
-        $cookie = [] ;
-        if(is_array($cookies)){
-            foreach ($cookies as $name=>$value){
-                $cookie[] = $name."=".$value;
+    public function setCookie($cookies)
+    {
+        $cookie = [];
+        if (is_array($cookies)) {
+            foreach ($cookies as $name => $value) {
+                $cookie[] = $name . "=" . $value;
             }
-            $cookies = implode(';',$cookie);
+            $cookies = implode(';', $cookie);
         }
 
         curl_setopt($this->curl, CURLOPT_COOKIE, $cookies);
@@ -69,18 +77,19 @@ class Curl{
     /**
      * 返回所有的链接数组
      */
-    public function getLinks(){
+    public function getLinks()
+    {
         $regex = "/<a href=\"([^\'\"]*)\" [^>]*>(.*?)<\/a>/";
-        preg_match_all($regex,$this->getContent(),$matchs);
+        preg_match_all($regex, $this->getContent(), $matchs);
         $links = [];
-        foreach($matchs[1] as $k=>$href){
+        foreach ($matchs[1] as $k => $href) {
             //去除掉空连接
-            if(substr($href,0,12)!="javascript:;"){
+            if (substr($href, 0, 12) != "javascript:;") {
                 //补全链接地址
-                if(substr($href,0,4)!="http"){
-                    $href=$this->url.$href;
+                if (substr($href, 0, 4) != "http") {
+                    $href = $this->url . $href;
                 }
-                array_push($links,$href);
+                array_push($links, $href);
             }
         }
         return $links;
@@ -90,63 +99,39 @@ class Curl{
      * get请求
      * @return mixed
      */
-    public function get($data){
+    public function get($data)
+    {
         $this->setData($data);
         return $this->getContent();
     }
 
     /**
-     * post请求
+     * post请求  文件img=>@file_path
      * @param $data
      * @return mixed
      */
-    public function post($data){
-        $this->setPost(1);
+    public function post($data)
+    {
+        $this->setPost();
+        $this->format($data);
         $this->setData($data);
+//        p($data);die;
         return $this->getContent();
     }
 
-
-    /**
-     * 单文件上传,默认根据表单上传
-     * @param $field
-     * @param string $filepath
-     * @return mixed
-     */
-    public function uploadFile($field,$filepath=""){
-        $data = [];
-        if(empty($filepath)){
-            //有文件上传
-            if(isset($_FILES[$field])){
-                //多文件情况
-                if(is_array($_FILES[$field]['name'])){
-                    $count = $_FILES[$field]['name'];
-                    foreach($_FILES[$field]['error'] as $k=>$v){
-                        //上传文件没有出错
-                        $v==0 && $data[$field.$k]='@'.$_FILES[$field]['tmp_name'][$k];
-                    }
-                }else{//单文件情况
-                    $_FILES[$field]['error'] == 0 && $data[$field]='@'.$_FILES[$field]['tmp_name'];
-                }
-            }
-        }else{
-            $data = [$field=>'@'.$filepath];
-        }
-        $this->filter($data);
-        return $this->post($data);
-    }
 
     /**
      * 下载文件
      * @param $url
      * @param $save_path
      */
-    public function downFile($url,$save_path){
+    public function downFile($url, $save_path)
+    {
         $ourl = $this->url;
         $this->setUrl($url);
         $this->setTimeOut(0);
         $filecontent = $this->getContent();
-        file_put_contents($save_path,$filecontent);
+        file_put_contents($save_path, $filecontent);
         $this->setUrl($ourl);
     }
 
@@ -156,30 +141,54 @@ class Curl{
      * @param $time
      * @return $this
      */
-    public function setTimeOut($time=30){
+    public function setTimeOut($time = 30)
+    {
         curl_setopt($this->curl, CURLOPT_TIMEOUT, $time);
         return $this;
     }
 
-    public function getContent(){
+    public function getContent()
+    {
         return curl_exec($this->curl);
     }
 
-    public function close(){
+    public function close()
+    {
         return curl_close($this->curl);
     }
 
-    public function __destruct(){
+    public function __destruct()
+    {
         $this->close();
     }
 
-    protected function filter(&$data){
-        if(version_compare(phpversion(),"5.5") > 0){
-            foreach ($data as $name=>$path){
-                $data[$name] = new CURLFile(ltrim($path,'@'));
+
+    /**
+     * 格式化请求数据，使符合要求，支持数组和单、多文件
+     * @param $data
+     * @return $this
+     */
+    protected function format(&$data)
+    {
+        $is_gt = version_compare(phpversion(), "5.5");
+
+        $is_gt || curl_setopt($this->curl, CURLOPT_SAFE_UPLOAD, false);
+
+        foreach ($data as $field => $value) {
+            if (is_string($value) && $value[0] == '@' && $is_gt) {
+                $data[$field] = new CURLFile(ltrim($value, '@'));
             }
-        }else{
-            curl_setopt($this->curl,CURLOPT_SAFE_UPLOAD, false);
+
+            if (is_array($value)) {
+                foreach ($value as $k => $v) {
+                    if ($v[0] == '@' && $is_gt) {
+                        $data[$field . "[$k]"] = new CURLFile(ltrim($v, '@'));
+                    } else {
+                        $data[$field . "[$k]"] = $v;
+                    }
+                }
+                unset($data[$field]);
+            }
         }
         return $this;
     }
@@ -187,11 +196,13 @@ class Curl{
     /**
      * 设置请求方式 post 默认为1 开启post，或者为get
      */
-    protected function setPost($status=1){
-        return curl_setopt($this->curl,CURLOPT_POST, 1);
+    protected function setPost($status = 1)
+    {
+        return curl_setopt($this->curl, CURLOPT_POST, $status);
     }
 
-    protected function setData($data){
+    protected function setData($data)
+    {
         return curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
     }
 }
