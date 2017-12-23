53130a18faef55aaa383d73f02cfce3434ac1629
apple <apple@appledeiMac.local>
2017-12-23 14:46:06 +0800

    数据缓存类

--- curl/demo.php
+++ curl/demo.php
@@ -1,3 +1,4 @@
+<title>接口测试</title>
 <style>
     .main{
         padding:10px;
@@ -24,14 +25,8 @@
         color: red;
     }
 </style>
-<div>
-    <form action="" enctype="multipart/form-data" method="post" multiple="multiple" >
-        <input type="file" name="image">
-        <input type="submit">
-    </form>
-</div>
-<?php
 
+<?php
 include("../functions.php");
 include("Curl.class.php");
 set_time_limit(0);
@@ -42,30 +37,40 @@ class Preach{
     private $domain = "http://preach.com/api/";
     private $image = '@/Users/apple/Downloads/59cb3a588efb7.png';
     private $video = '@/Users/apple/Downloads/audio.mp3';
+    private $audio = '@/Users/apple/Downloads/yinyue.mp3';
+    private $course_id;
+    private $chapter_id;
+    private $course_ware_id;
     private $curl ;
     private $reflect;
     private $allowControllerIds = [
-//        'banners',
-//        'categories',
-//        'chapters',
-//        'cities',
-//        'courses',
-//        'course_wares',
-//        'devices',
-        'messages',
-//        'productChannels',
-//        'provinces',
-//        'push',
 //        'sale_zones',
 //        'soft_versions',
+//        'banners',
+//        'categories',
+//        'productChannels',
+//        'provinces',
+//        'cities',
+
+        'courses',
+//        'chapters',
+
+//        'devices',
+
+//        'messages',
+
+//        'push',
+
 //        'teachers',
+//        'course_wares',
+
 //        'users'
     ];
     private $umaskMethods = [
         '__construct',
         'exec',
         'users_logout',
-//        'users_login',
+        'users_login',
     ];
 
     public function __construct()
@@ -108,6 +113,18 @@ class Preach{
 
     private function setSid($data){
         isset($data['sid']) && $this->common['sid'] = $data['sid'];
+    }
+
+    private function setCourseId($id){
+        $this->course_id = $id;
+    }
+
+    private function setChapterId($id){
+        $this->chapter_id = $id;
+    }
+
+    private function setCourseWareId($id){
+        $this->course_ware_id = $id;
     }
 
     private function basicRequest($data,$url,$type='basic',$method='get'){
@@ -190,62 +207,70 @@ class Preach{
     //用户接口
 
     //请求验证码
-    public function users_sendAuth($url){
-        $this->post([],$url);
-    }
+//    public function users_sendAuth($url){
+//        $this->post([],$url);
+//    }
+//
+//
+//    //登录
+//    public function users_login($url){
+//        $data = $this->post([],$url);
+//        $this->setSid($data);
+//    }
+//
+//    public function users_logout($url){
+//        $this->post([],$url);
+//    }
+//
+//    public function users_detail($url){
+//        $this->get([],$url);
+//    }
 
-
-    //登录
-    public function users_login($url){
-        $data = $this->post([],$url);
-        $this->setSid($data);
-    }
-
-    public function users_logout($url){
-        $this->post([],$url);
-    }
-
-    public function users_detail($url){
+    public function users_playHistories($url){
         $this->get([],$url);
     }
 
 
-    //老师课程接口
+    //新增系列课程接口
+    public function teachers_courseCreate($url){
+        $data = [
+            'title' => time()."测试课程创建",
+            'chapter_num'  => rand(3,8),
+            'category_id'  => 1,
+            'second_category_id' => 3,
+            'price' => rand(10,38),
+        ];
+        $resp_data = $this->post($data,$url);
+        $this->setCourseId($resp_data['id']);
+        $this->call('teachers_courseDetail');
+    }
+
+    //系列课更新接口
+    public function teachers_courseUpdate($url){
+        $data = [
+            'course_id' => $this->course_id,
+            'title' => time().'测试课程更新',
+            'description'=>time().'测试课程描述',
+            'price' => rand(100,200),
+            'surface_image' => $this->image,
+            'video' => $this->video,
+            'video_image' => $this->image,
+            'description_image_num'=>1,
+            'surface_image' => $this->image,
+            'description_image_0' => $this->image
+        ];
+        $this->post($data,$url);
+//        $this->call('teachers_courseDetail');
+    }
+
+    //老师系列课程详情接口
     public function teachers_courseDetail($url){
         $data = [
-            'id' => 1
+            'course_id' => $this->course_id
         ];
         $this->get($data,$url);
     }
 
-    //新增课程接口
-//    public function teachers_courseCreate($url){
-//        $data = [
-//            'title' => time()."测试课程创建",
-//            'chapter_num'  => rand(3,8),
-//            'category_id'  => 1,
-//            'second_category_id' => 3,
-//            'price' => rand(10,38)
-//        ];
-//        $this->post($data,$url);
-//    }
-
-
-//    public function teachers_courseUpdate($url){
-//        $data = [
-//            'id' => 1,
-//            'title' => time().'测试课程title',
-//            'description'=>time().'测试课程描述',
-//            'price' => rand(100,200),
-//            'description_image_num'=>3,
-//            'surface_image' => $this->image,
-//            'description_image_0' => $this->image,
-//            'description_image_1' => $this->image,
-//            'description_image_2' => $this->image,
-//            'video_image' => $this->image
-//        ];
-//        $this->post($data,$url);
-//    }
 
     //删除课程介绍图片
 //    public function teachers_courseImageDelete($url){
@@ -271,64 +296,93 @@ class Preach{
 //        $this->post($data,$url);
 //    }
 
-    //章节列表接口
-    public function teachers_chapters($url){
+    //新增章节接口
+    public function teachers_chapterCreate($url){
         $data = [
-            'course_id' => 1
+            'course_id' => $this->course_id,
+            'title' => time()."测试章节创建",
+            'category_id'  => 1,
+            'second_category_id' => 3,
+            'price' => rand(10,38),
+            'publis_at' => time()
         ];
-        $this->get($data,$url);
+        $resp_data = $this->post($data,$url);
+        $this->setChapterId($resp_data['id']);
+//        $this->call('teachers_chapterDetail');
+//        $this->call('teachers_courseDetail');
     }
+
+    //更新章节详情接口
+//    public function teachers_chapterUpdate($url){
+//        $data = [
+//            'chapter_id' => $this->chapter_id,
+//            'title' => time().'测试章节更新'
+//        ];
+//        $this->post($data,$url);
+//    }
 
     //章节详情接口
     public function teachers_chapterDetail($url){
         $data = [
-            'id' => 1
+            'chapter_id' => $this->chapter_id
         ];
         $this->get($data,$url);
     }
 
-    //新增章节接口
-//    public function teachers_chapterCreate($url){
-//        $data = [
-//            'id' => 1
-//        ];
-//        $this->post($data,$url);
-//    }
-
-    //更新章节详情接口
-    public function teachers_chapterUpdate($url){
+    //课程章节列表接口
+    public function teachers_chapters($url){
         $data = [
-            'id' => 1,
-            'title' => time().'测试章节title'
+            'course_id' => $this->course_id
         ];
-        $this->post($data,$url);
+        $this->get($data,$url);
     }
 
 
     //课件列表接口
-    public function courseWares($url){
-        $data = [
-            'chapter_id' => 1
-        ];
-        $this->get($data,$url);
-    }
+//    public function courseWares($url){
+//        $data = [
+//            'chapter_id' => $this->chapter_id
+//        ];
+//        $this->basicGet($data,$url);
+//    }
 
     //新增课件接口
-//    public function courseWares_create($url){
-//        $data = [
-//            'chapter_id' => 1,
-//            'image' => $this->image,
-//            'audio' => $this->video
-//        ];
-//        $this->post($data,$url);
-//    }
+    public function courseWares_create($url){
+        $data = [
+            'chapter_id' => $this->chapter_id,
+            'name' => time().'新增课件',
+            'image' => $this->image,
+            'video' => $this->video,
+            'audio' => $this->video,
+        ];
+        $resp_data = $this->post($data,$url);
+        $this->setCourseWareId($resp_data['id']);
+//        $this->call('courseWares');
+//        $this->call('teachers_courseDetail');
+    }
 
     //更新课件接口
 //    public function courseWares_update($url){
 //        $data = [
-//            'id' => 1,
+//            'id' => $this->course_ware_id,
 //            'image' => $this->image,
-//            'audio' => $this->video
+//            'name' => time().'更新课件',
+//        ];
+//        $this->post($data,$url);
+//    }
+
+    //课件详情接口
+    public function courseWares_detail($url){
+        $data = [
+            'id' => $this->course_ware_id,
+        ];
+        $this->post($data,$url);
+    }
+
+    //排序列表接口
+//    public function courseWares_sort($url){
+//        $data = [
+//            'ids' => "[".$this->course_ware_id."]"
 //        ];
 //        $this->post($data,$url);
 //    }
@@ -336,41 +390,27 @@ class Preach{
     //删除课件接口
 //    public function courseWares_delete($url){
 //        $data = [
-//            'id' => 1
+//            'id' => $this->course_ware_id
 //        ];
 //        $this->post($data,$url);
+//        $this->call('courseWares');
 //    }
 
-    //排序列表接口
-//    public function courseWares_sort($url){
-//        $data = [
-//            'ids' => [
-//                23,24,46
-//            ]
-//        ];
-//        $this->post($data,$url);
-//    }
 
-    //课件详情接口
-//    public function courseWares_detail($url){
-//        $data = [
-//            'id	' => 1
-//        ];
-//        $this->post($data,$url);
-//    }
+
 
     //我的提问列表
-//    public function messages_user($url){
-//        $data = [
-//            'course_id' => 1
-//        ];
-//        $this->get($data,$url);
-//    }
+    public function messages_user($url){
+        $data = [
+            'course_id' => 42
+        ];
+        $this->get($data,$url);
+    }
 
     //房间的问题列表
     public function messages_room($url){
         $data = [
-            'course_id' => 1
+            'course_id' => 42
         ];
         $this->post($data,$url);
     }
@@ -379,29 +419,29 @@ class Preach{
     //提问接口
     public function messages_mail($url){
         $data = [
-            'chapter_id' => 1,
-            'content' => ''
+            'chapter_id' => 29,
+            'content' => 'fsdfdsfsdf'
         ];
         $this->post($data,$url);
     }
 
 
     //老师的课程问题列表(老师入口)
-//    public function messages_course($url){
-//        $data = [
-//            'course_id' => 1
-//        ];
-//        $this->get($data,$url);
-//    }
+    public function messages_course($url){
+        $data = [
+            'course_id' => 1
+        ];
+        $this->get($data,$url);
+    }
 
     //回复问题接口(老师入口)
-    public function messages_reply($url){
-        $data = [
-            'first_message_id' => 1,
-            'content' => '回复测试信息'
-        ];
-        $this->post($data,$url);
-    }
+//    public function messages_reply($url){
+//        $data = [
+//            'first_message_id' => 1,
+//            'content' => '回复测试信息'
+//        ];
+//        $this->post($data,$url);
+//    }
 
 
     //获取Banners接口
@@ -451,7 +491,7 @@ class Preach{
     //课程详情接口
     public function courses_detail($url){
         $data = [
-            'id' => 1
+            'id' => 42
         ];
         $this->basicGet($data,$url);
     }
@@ -485,32 +525,42 @@ class Preach{
     //播放上报接口
     public function chapters_play($url){
         $data = [
-            'course_ware_id' => 49
+            'course_ware_id' => 100
         ];
         $this->post($data,$url);
+    }
+
+    private function call($function_name){
+        $temp = explode('_',$function_name);
+        $controller_id = $this->camelToUnderLine($temp[0]);
+        if(in_array($controller_id,$this->allowControllerIds)){
+            if(isset($temp[1])){
+                $action_id = $this->camelToUnderLine($temp[1]);
+                $url = $controller_id.'/'.$action_id;
+            }else{
+                $url = $controller_id;
+            }
+            call_user_func([get_called_class(),$function_name],$url);
+        }
     }
 
     public function exec(){
         $methods = $this->reflect->getMethods();
         foreach($methods as $method){
             if(!in_array($method->name,$this->umaskMethods) && $method->isPublic()){
-                $temp = explode('_',$method->name);
-                $controller_id = $this->camelToUnderLine($temp[0]);
-                if(in_array($controller_id,$this->allowControllerIds)){
-                    if(isset($temp[1])){
-                        $action_id = $this->camelToUnderLine($temp[1]);
-                        $url = $controller_id.'/'.$action_id;
-                    }else{
-                        $url = $controller_id;
-                    }
-                    call_user_func([$method->class,$method->name],$url);
-                }
+                $this->call($method->name);
             }
         }
     }
 
 }
-
 $preach = new Preach();
-//$preach->exec();
+$preach->exec();
+//
+//p($_FILES);
+//
+//$curl = new Curl("http://study.com/curl/upload.php");
+//$curl->setTempDir("./temp");
+//p($curl->transferQuery());
+
 ?>
