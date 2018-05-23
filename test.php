9cb40dbbda00f390c17c7f31e24d18760f2b96bf
apple <1907928206@qq.com>
2018-05-23 08:21:54 +0800

    清理缓存

--- test.php
+++ test.php
@@ -1,30 +1,3 @@
 <?php
-$rk = new RdKafka\Consumer();
-$rk->setLogLevel(LOG_DEBUG);
-$rk->addBrokers('localhost:9092');
-$offset = 1;
-$topic = $rk->newTopic('test');
-$partition = 0;
-$topic->consumeStart($partition, 0);
-while (true) {
-    $message = $topic->consume($partition, 3);
-    if (!is_object($message)) {
-        continue;
-    }
-    switch ($message->err) {
-        case RD_KAFKA_RESP_ERR_NO_ERROR:
-            print_r($message);
-            break;
-        case RD_KAFKA_RESP_ERR__PARTITION_EOF:
-            echo 'eof' . PHP_EOL;
-            sleep(1);
-            break;
-        case RD_KAFKA_RESP_ERR__TIMED_OUT:
-            echo 'out' . PHP_EOL;
-            sleep(1);
-            break;
-        default:
-            throw new \Exception($message->errstr(), $message->err);
-            break;
-    }
-}
+
+print_r(['name' => 'ding', 'age' => 23]);
