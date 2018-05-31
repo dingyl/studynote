7a2da67f35ddcb55190199cdf7a2c32faf4080da
dingyaoli <1907928206@qq.com>
2018-05-31 22:23:43 +0800

    消息队列添加

--- kafka/producer.php
+++ kafka/producer.php
@@ -1,7 +1,17 @@
 <?php
-$rk = new RdKafka\Producer();
-$rk->setLogLevel(LOG_DEBUG);
-$rk->addBrokers('localhost:9092');
-$theme = "test";
-$topic = $rk->newTopic($theme);
-$topic->produce(RD_KAFKA_PARTITION_UA, 0, $theme . "Message " . time());
+//$rk = new RdKafka\Producer();
+//$rk->setLogLevel(LOG_DEBUG);
+//$rk->addBrokers('localhost:9092');
+//$theme = "test";
+//$topic = $rk->newTopic($theme);
+//$topic->produce(RD_KAFKA_PARTITION_UA, 0, $theme . "Message " . time());
+//
+
+
+require_once "KafkaQueue.php";
+$host = '127.0.0.1:9092';
+$name = 'test';
+
+$queue = KafkaQueue::getInstance($host, $name);
+
+$queue->push('dingyaoli');
--- kafka/test.php
+++ kafka/test.php
@@ -10,5 +10,19 @@ $queue = KafkaQueue::getInstance($host, $name);
 
 while(1)
 {
-    $message = $queue->pop();
+    $message = $message = $queue->pop();
+    switch ($message->err) {
+        case RD_KAFKA_RESP_ERR_NO_ERROR:
+            var_dump($message);
+            break;
+        case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+            echo "No more messages; will wait for more\n";
+            break;
+        case RD_KAFKA_RESP_ERR__TIMED_OUT:
+            echo "Timed out\n";
+            break;
+        default:
+            throw new \Exception($message->errstr(), $message->err);
+            break;
+    }
 }
